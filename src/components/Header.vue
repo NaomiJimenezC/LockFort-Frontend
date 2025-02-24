b1f379a527f7cf5924a5ddeb4191af6ef131fd0d
NaomiJimenezC <ijimenez@iesrafaelalberti.es>
2025-02-24 18:00:41 +0100

    added icons and made header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,9 +5,20 @@ export default {
 </script>
 
 <template>
-
+  <img src="../assets/logo.svg" alt="logo de la página" />
+  <h1>LockFort</h1>
+  <select>
+    <option>Generador</option>
+    <option>Comprobador</option>
+  </select>
+  <ul>
+    <li><img src="../assets/lightbulb_24dp_46152F.svg"></li>
+    <li><img src="../assets/dashboard_24dp_46152F.svg"></li>
+    <li><img src="../assets/login_24dp_46152F.svg"></li>
+    <li><img src="../assets/account_circle_24dp_46152F.svg"></li>
+  </ul>
 </template>
 
-<style scoped>
+<style>
 
 </style>
